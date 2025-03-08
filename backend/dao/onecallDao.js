0db186048b52a099933a4b0659721d183fed61bc
Louis Volant <volantlouis@gmail.com>
2025-03-08 12:43:36 +0100

    Changed the way to import MONGO_URI, building it instead of retrieving it

--- backend/dao/onecallDao.js
+++ backend/dao/onecallDao.js
@@ -1,7 +1,13 @@
+// dao/oncallDao.js
 const mongoose = require('mongoose');
 
-const MONGO_URI = "mongodb+srv://"+process.env.MONGODB_ATLAS_USERNAME+":"+MONGODB_ATLAS_PASSWORD+"@"+process.env.MONGODB_ATLAS_CLUSTER_URL+"/"+process.env.MONGODB_ATLAS_DB_NAME+"?retryWrites=true&w=majority&appName="+MONGODB_ATLAS_APP_NAME
-console.log('Mongo URI:', process.env.MONGO_URI);
+const MONGO_URI = "mongodb+srv://" +
+    process.env.MONGODB_ATLAS_USERNAME+ ":" +
+    process.env.MONGODB_ATLAS_PASSWORD + "@" +
+    process.env.MONGODB_ATLAS_CLUSTER_URL + "/" +
+    process.env.MONGODB_ATLAS_DB_NAME + "?retryWrites=true&w=majority&appName=" +
+    process.env.MONGODB_ATLAS_APP_NAME
+//console.log('Mongo URI:', MONGO_URI);
 
 // Connect to MongoDB using Mongoose
 mongoose.connect(MONGO_URI)
