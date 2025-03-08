764e18c824095c744b54d681557d42a8abb46d09
Louis Volant <volantlouis@gmail.com>
2025-03-08 10:36:08 +0100

    Splitted env variables in MONGO_URI to try fixing the issues with encoded characters.

--- backend/dao/searchDao.js
+++ backend/dao/searchDao.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
+const MONGO_URI = "mongodb+srv://"+process.env.MONGODB_ATLAS_USERNAME+":"+MONGODB_ATLAS_PASSWORD+"@"+process.env.MONGODB_ATLAS_CLUSTER_URL+"/"+process.env.MONGODB_ATLAS_DB_NAME+"?retryWrites=true&w=majority&appName="+MONGODB_ATLAS_APP_NAME
+console.log('Mongo URI:', process.env.MONGO_URI);
+
 // Connect to MongoDB using Mongoose
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(MONGO_URI)
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.error('MongoDB Connection Error:', err));
 
