4808f56b7d05adeb810b0aa78f08aa526eccfbfd
Louis Volant <volantlouis@gmail.com>
2025-05-16 20:58:00 +0100

    Improved logo for Google Login

--- frontend/src/app/components/LoginModal.tsx
+++ frontend/src/app/components/LoginModal.tsx
@@ -1,3 +1,4 @@
+// src/components/LoginModal.tsx
 'use client';
 
 import { useState, useEffect } from 'react';
@@ -114,22 +115,30 @@ export default function LoginModal({ isOpen, setIsOpen }: LoginModalProps) {
             onClick={handleGoogleLogin}
             className="w-full flex items-center justify-center gap-2 bg-white dark:bg-gray-700 text-gray-700 dark:text-gray-200 border border-gray-300 dark:border-gray-600 hover:bg-gray-100 dark:hover:bg-gray-600 px-4 py-2 rounded-md shadow-sm transition-all duration-300"
           >
-            <svg className="w-5 h-5" viewBox="0 0 24 24">
+            <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-5 h-5">
               <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M17.6996 9.20184C17.6996 8.57234 17.6432 7.96706 17.5382 7.38599H9.17725V10.82H13.955C13.7492 11.9297 13.1237 12.8699 12.1835 13.4994V15.7268H15.0525C16.7312 14.1813 17.6996 11.9054 17.6996 9.20184Z"
                 fill="#4285F4"
-                d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
               />
               <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M9.1774 17.8775C11.5743 17.8775 13.5839 17.0826 15.0527 15.7268L12.1836 13.4993C11.3887 14.032 10.3718 14.3467 9.1774 14.3467C6.86521 14.3467 4.90813 12.7851 4.21003 10.6868H1.24414V12.9868C2.70489 15.8882 5.7071 17.8775 9.1774 17.8775Z"
                 fill="#34A853"
-                d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-癌症.02.68-2.31 1.08-3.71 1.08-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"
               />
               <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M4.20994 10.687C4.03239 10.1543 3.93151 9.58534 3.93151 9.00023C3.93151 8.41512 4.03239 7.84616 4.20994 7.31351V5.01343H1.24405C0.642799 6.21189 0.299805 7.56773 0.299805 9.00023C0.299805 10.4327 0.642799 11.7886 1.24405 12.987L4.20994 10.687Z"
                 fill="#FBBC05"
-                d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"
               />
               <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M9.1774 3.65338C10.4808 3.65338 11.651 4.10129 12.571 4.98097L15.1173 2.43474C13.5798 1.00224 11.5703 0.122559 9.1774 0.122559C5.7071 0.122559 2.70489 2.11193 1.24414 5.01326L4.21003 7.31334C4.90813 5.21502 6.86521 3.65338 9.1774 3.65338Z"
                 fill="#EA4335"
-                d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 2.47 2.18 5.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
               />
             </svg>
             <span>Log in with Google</span>
